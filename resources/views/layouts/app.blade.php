66c750f7011678f6a576261973287097b63abc32
Aironman Syakir <aironmansyakir@gmail.com>
2024-06-04 02:31:32 +0800

    Navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,13 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+            @if (Auth::user()->userType == 'admin')
+            {
+                @include('layouts.adminnavigation')
+            }
+            @else
+            {@include('layouts.navigation')}
+            @endif
 
             <!-- Page Heading -->
             @if (isset($header))
